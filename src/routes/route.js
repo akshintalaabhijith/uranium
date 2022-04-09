13ba24220cb1a75659db2743f46cdf20478d4fed
akshintalaabhijith <akshintala.abhijith@gmail.com>
2022-04-09 15:38:06 +0530

    post request assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,13 +3,34 @@ const express = require("express");
 const router = express.Router();
 
 let players = [
-  {
-    name: "manish",
-    dob: "1/1/1995",
-    gender: "male",
-    city: "jalandhar",
-    sports: ["swimming"],
-  },
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+
 ];
 router.post("/players", function (req, res) {
   for (let i = 0; i < players.length; i++) {
@@ -23,7 +44,7 @@ router.post("/players", function (req, res) {
   players.push(user);
   console.log(players);
 
-  res.send(players);
+  res.send({ data: players , status: true });
 });
 
 module.exports = router;

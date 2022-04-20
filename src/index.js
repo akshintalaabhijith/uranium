31a195ad25f783c3171df3f828bbe4180522c9e2
akshintalaabhijith <akshintala.abhijith@gmail.com>
2022-04-20 17:25:31 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment = require('moment')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next){
+    console.log(moment().format(), req.path, req.ip)
+    next()
+})
 
 
 mongoose.connect("mongodb+srv://Abhijith:Abhijith@cluster0.w7nwz.mongodb.net/FunctionUp?authSource=admin&replicaSet=atlas-darc46-shard-0&readPreference=primary&ssl=true", {
@@ -13,6 +19,8 @@ mongoose.connect("mongodb+srv://Abhijith:Abhijith@cluster0.w7nwz.mongodb.net/Fun
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
+
+
 
 app.use('/', route);
 
